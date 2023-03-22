54dce21b5094aeace97bdbaf018922126a87071b
Santu1317 <128033779+Santu1317@users.noreply.github.com>
2023-03-22 19:08:37 +0530

    Add files via upload

---
+++ day 3/1.py
@@ -0,0 +1,107 @@
+'''n1=int(input("enter a value for n1"))
+n2=int(input("enter a value for n2"))
+array=[i for i in range(n1,n2+1)]
+print(array)
+l1=[array[i:j+1] for i in range(len(array))for j in eange(i,len(array))]
+print(l1)
+c=0
+for i in l1:
+   if sum(i)%2!=0:
+    c+=1
+print(c)'''
+#------------------------------------------
+
+#list comprehension
+#for loop version
+'''result=[]
+for i in range(11):
+    result.append(i)
+print(result)
+#list comprehension version
+print([i for i in range(11)])'''
+#------------------------------------------
+
+'''result=[]
+for i in range(11):
+    if i%2!=0:
+        result.append(i)
+print(result)
+print([i for i in range(11) if i%2!=0])'''
+#------------------------------------------
+
+'''result=[]
+for i in range(11):
+    if i%2!=0:
+        result.append(i)
+    else:
+        result.append(i**2)
+print(result)
+print([i if i%2!=0 else i**2 for i in range(11)])'''
+#-------------------------------------------------------
+
+'''mat=[[1,2,3,4],[5,6,7,8,],[9,10,11,12],[13,14,15,16]]
+result=[]
+for i in range(len(mat)):
+    for j in mat[i]:
+        if j%2!=0:
+           result.append(j**2)
+        else:
+           result.append(j**3)
+print(result)
+print([j**2 if j%2!=0 else j**3 for i in range(len(mat)) for j in mat[i]])'''
+#---------------------------------------------------------------------------------
+
+'''mat=[[1,2,3,4],[5,6,7,8,],[9,10,11,12],[13,14,15,16]]
+result=[]
+for row in mat:
+    row_data=[]
+    for ele in row:
+        if ele%2!=0:
+           row_data.append(ele**2)
+        else:
+           row_data.append(ele**3)
+print(result)
+print([ele**2 if ele%2!=0 else ele**3 for row in mat for ele in row])
+print([[ele**2 if ele%2!=0 else ele**3 for ele in row] for row in mat])'''
+#-----------------------------------------------------------------------------------
+
+'''mylist=[9,3,6,1,5,0,8,2,4,7]
+list_b=[6,4,6,1,2,2]
+result=[]
+for i in list_b:
+    if i in mylist:
+        a=(i,mylist.index(i))
+    result.append(a)
+print(result)
+print([(i,mylist.index(i)) for i in list_b if i in mylist])'''  
+#------------------------------------------------------------------------
+
+#dictionary comprehension
+'''mylist=[9,3,6,1,5,0,8,2,4,7]
+list_b=[6,4,6,1,2,2]
+result={}
+for i in list_b:
+    result[i]=mylist.index(i)
+print(result)
+print({i:mylist.index(i) for i in list_b})'''
+#------------------------------------------------------------------
+
+sentences=["a new world record was set",
+           "in the holy city of ayodhya",
+           "on the eve of diwali on tuesday",
+           "with over three lakh diya or earthen lamps",
+           "list up simultaneously on the banks of the salary river"]
+stopwords=['for','a','of','the','and','to','in','on','with']
+result=[]
+for sentence in sentences:
+    row_data=[]
+    for word in sentence.split():
+       print(word)
+       if word not in stopwords:
+           row_data.append(word)
+    result.append(row_data)
+print(result)
+print([word for word in sentence.split() if word not in stopwords for sentence in sentences])
+#-----------------------------------------------
+
+
